ece615d6ebf7b4340ca992f28b89544d3adb5462
Donkey <danglayers@gmail.com>
2020-08-04 14:58:45 -0400

    added connection to MYSQL + created inquirer prompt

--- server.js
+++ server.js
@@ -1,2 +1,60 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3000,
+    user: "root",
+    password: "",
+    database: "employee_trackerDB"
+});
+
+// connect to mysql server
+connection.connect(function(err) {
+    if (err) throw err;
+    startPrompt();
+});
+
+function startPrompt() {
+    inquirer
+      .prompt({
+          type: "list",
+          name: "task",
+          message: "What would you like to do?",
+          choices: [
+              "View Employees",
+              "View Employees by Department",
+              "Add Employee",
+              "Remove Employee",
+              "Update Employee Information",
+              "Add role",
+              "End"
+          ]
+      })
+      .then(function ({ task }) {
+          switch (task) {
+              case "View Employees":
+                  viewEmployee();
+                  break;
+              case "View Employees by Department":
+                  viewEmployeeByDepartment();
+                  break;
+              case "Add Employee":
+                  addEmployee();
+                  break;
+              case "Remove Employee":
+                  removeEmployee();
+                  break;
+              case "Update Employee Information":
+                  updateEmployeeRole();
+                  break;
+              case "Add Role":
+                  addRole();
+                  break;
+              case "End":
+                  connection.end();
+                  break;
+          }
+      });
+}
